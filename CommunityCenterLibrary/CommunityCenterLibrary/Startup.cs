e8abcde252af4fb0432f4143f94a42cde8f22e54
rmzslfv <rmzslfv@abv.bg>
2016-05-15 22:06:13 +0300

    I created Library ASP.NET app

---
+++ CommunityCenterLibrary/CommunityCenterLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityCenterLibrary.Startup))]
+namespace CommunityCenterLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
