6dfd6f105af47b07c5bd3e25711f819488b8f151
rmzslfv <rmzslfv@abv.bg>
2016-05-26 22:07:54 +0300

    Books Model and Local Database

---
+++ CommunityCenterLibrary/CommunityCenterLibrary/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CommunityCenterLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public DateTime Date { get; set; }
+
+
+    }
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
